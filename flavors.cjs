2f7246e2cc97f379041dd5a41a4f5b897ad5c67f
Darren Lewis <darrenlewis@Darrens-MBP.attlocal.net>
2024-08-07 13:04:07 -0400

    new tables

---
+++ flavors.cjs
@@ -0,0 +1,55 @@
+const client = require('./client.cjs')
+const createFlavor = async(name, is_favorite) => {
+  try {
+    const { rows } = await client.query (`
+      INSERT INTO flavors (name, is_favorite)
+      VALUES ('${name}', '${is_favorite}')
+      RETURNING *;
+    `);
+    return rows;
+  } catch(e) {
+     return e;
+  }
+}
+
+const updateFlavor = async(id, name, is_favorite) => {
+  try {
+    const { rows } = await client.query (`
+      UPDATE flavors
+      SET name='${name}', is_favorite='${is_favorite}', updated_at=CURRENT_TIMESTAMP
+      WHERE id=${id}
+      RETURNING *;
+    `);
+    return rows;
+  } catch(e) {
+     return e;
+  }
+}
+
+const getFlavors = async() => {
+  try {
+    const { rows } = await client.query (`SELECT * FROM flavors;`);
+    return rows;
+  } catch(e) {
+    console.log(e);
+  }
+}
+
+const getSpecificFlavor = async(id) => {
+  try {
+    const { rows } = await client.query (`SELECT * FROM flavors WHERE id=${id}`);
+    return rows;
+  } catch(e) {
+    console.log(e);
+  }
+}
+
+const deleteSpecificFlavor = async(id) => {
+  try {
+    await client.query (`DELETE FROM flavors WHERE id=${id}`);
+  } catch(e) {
+    console.log(e);
+  }
+}
+
+module.exports = { createFlavor, getFlavors, getSpecificFlavor, deleteSpecificFlavor, updateFlavor }
---
+++ seed.cjs
@@ -0,0 +1,49 @@
+const client = require('./client.js')
+
+const { createFlavor } = require('./flavors.cjs');
+
+const dropTables = async() => {
+  try {
+    await client.query(`DROP TABLE IF EXISTS flavors;`)
+  } catch(e) {
+    console.log(e);
+  }
+}
+
+const createTables = async() => {
+  try {
+    await client.query(`
+      CREATE TABLE flavors (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(20) NOT NULL UNIQUE,
+        is_favorite BOOLEAN NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP
+      );
+    `)
+  } catch(e) {
+    console.log(e);
+  }
+}
+
+const syncAndSeed = async() => {
+  await client.connect();
+  console.log(`connected`);
+
+  await dropTables();
+  console.log(`dropped tables`);
+
+  await createTables();
+  console.log(`created tables`);
+
+  await createFlavor(`chocolate`, true);
+  console.log(`created first flavor`);
+
+  await createFlavor(`vanilla`, false);
+  console.log(`created second flavor`);
+
+  await client.end();
+  console.log(`disconnected`);
+}
+
+syncAndSeed();
